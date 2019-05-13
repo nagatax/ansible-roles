add26d9494db5ae468b2042b5524612202e50e0e
nagatax <4foview@gmail.com>
2019-05-13 21:01:45 +0900

    goロールとmailhogロールの修正
    goロール:実行ファイルをダウンロードし、配置するように変更する / mailhogロール:インストールスクリプトを見直す

--- spec/go/go_spec.rb
+++ spec/go/go_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+# rootユーザの場合、PATHに"/usr/local/bin"が
+# 登録されていないので、登録する
+set :path, '/usr/local/bin:$PATH'
+
 describe command('which go') do
   its(:exit_status) { should eq 0 }
-end 
+end
+
+describe command('go version') do
+  its(:stdout) { should match /1.12.5/ }
+end
